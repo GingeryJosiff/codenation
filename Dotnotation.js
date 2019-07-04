7dc081a34481aefe449e1f927fe78663615da161
codenation <codenation@codenations-MacBook-Pro.local>
2019-07-04 12:01:13 +0100

    This is my Codenation file

---
+++ Dotnotation.js
@@ -0,0 +1,27 @@
+/*
+console.log("hello");
+
+console.log("hello".length);
+
+console.log(Math.random());
+
+console.log(Math.floor(Math.random()*11));
+
+// two forward slashes at start for a note
+forward slash asterisc to open HIDE, asterisc forward slash to close
+
+console.log("   |   |   ")
+console.log("   |   |   ")
+console.log("   |   |   ")
+console.log("-----------")
+console.log("   |   |   ")
+console.log("   |   |   ")
+console.log("   |   |   ")
+console.log("-----------")
+console.log("   |   |   ")
+console.log("   |   |   ")
+console.log("   |   |   ")
+*/
+
+
+console.log("All Around the World".toUpperCase().charAt());
